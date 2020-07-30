22d81f0089aa622878fbc130e49413f992937b68
Erick González Sánchez <erick.gonzalezs@outlook.com>
2020-07-29 21:13:55 -0500

    Se agrega HomeController
    Se agrega HomeController

---
+++ Altatec/ExamenPracticoApi/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿namespace ExamenPracticoApi.Controllers
+{
+  using Filters;
+  using Models.CustonExceptions;
+  using System;
+  using System.Web.Http;
+  [ExcepcionAltatecFilter]
+  public class HomeController : ApiController
+  {
+    [Route("")]
+    [Route("api")]
+    [Route("api/examenPracticoAltatec")]
+    public IHttpActionResult GetHome()
+    {
+      try
+      {
+        return Json("Examen práctico Altatec Erick González Sánchez");
+      }
+      catch (Exception e)
+      {
+        throw new AltatecCustomExceptionModel(e.Message);
+      }
+    }
+  }
+}
